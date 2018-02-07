1918aca81ea338e127d571cc780b00551e62a88b
Nersa Acosta <nersa.acosta@gmail.com>
2018-02-07 14:49:01 -0500

    Change demo

--- app/src/main/java/com/kickingmobiledev/customviewjava/ui/activities/Section10Activity.java
+++ app/src/main/java/com/kickingmobiledev/customviewjava/ui/activities/Section10Activity.java
@@ -2,12 +2,24 @@ package com.kickingmobiledev.customviewjava.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.view.View;
 
+import com.kickingmobiledev.customtextview.ResizableTextView;
 import com.kickingmobiledev.customviewjava.R;
+import com.kickingmobiledev.customviewjava.databinding.ActivitySection10Binding;
 
 public class Section10Activity extends AppCompatActivity {
+
+    private ResizableTextView textView;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, Section10Activity.class);
@@ -17,6 +29,16 @@ public class Section10Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section10);
+        ActivitySection10Binding binding = DataBindingUtil.setContentView(this, R.layout.activity_section10);
+        textView = binding.textView;
+    }
+
+    public void onClickButton(View button) {
+        SpannableString spannable = new SpannableString(textView.getText());
+        spannable.setSpan(new StrikethroughSpan(), 10, 13, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new StyleSpan(Typeface.ITALIC), 10, 13, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new UnderlineSpan(), 0, 5, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new StyleSpan(Typeface.BOLD), 8, textView.getText().length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        textView.setText(spannable);
     }
 }
